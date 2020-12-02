4605ad64933b51e2f3b9c007e99b14af68c4a5d3
Ørjan <orjellingsen@gmail.com>
2020-12-02 10:01:35 +0100

    add error handling to loadPage

--- wiki.go
+++ wiki.go
@@ -15,8 +15,11 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
